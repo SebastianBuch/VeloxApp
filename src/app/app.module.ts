53c950fb1e2b0dc2fde69f0ffb844c117defecc0
Rasmus Nielsen <rasmus.andreas96@gmail.com>
2017-12-04 10:20:49 +0100

    Scanner moved to menupage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import {MenuPage} from '../pages/menu/menu';
+import {StatsPage} from '../pages/stats/stats';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    MenuPage
+    MenuPage,
+    StatsPage
   ],
   imports: [
     BrowserModule,
@@ -23,7 +25,8 @@ import {MenuPage} from '../pages/menu/menu';
   entryComponents: [
     MyApp,
     HomePage,
-    MenuPage
+    MenuPage,
+    StatsPage
   ],
   providers: [
     StatusBar,
--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {AlertController, NavController, NavParams} from 'ionic-angular';
+import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
 import {HomePage} from '../home/home';
+import {StatsPage} from '../stats/stats';
 
 /**
  * Generated class for the MenuPage page.
@@ -15,15 +17,40 @@ import {HomePage} from '../home/home';
 })
 export class MenuPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController, private barcodeScanner: BarcodeScanner) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad MenuPage');
+  async scanBarcode() {
+    const results = await this.barcodeScanner.scan();
+    console.log(results);
   }
 
-  goToBack(){
-    this.navCtrl.setRoot(HomePage);
+  presentConfirm() {
+    let alert = this.alertCtrl.create({
+      title: 'Are you sure?',
+      message: 'You will not be able to scan products for this inventory anymore.',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Disconnect',
+          handler: () => {
+            this.navCtrl.setRoot(HomePage);
+            console.log('Buy clicked');
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  goToStats() {
+    this.navCtrl.push(StatsPage);
   }
 
 }
