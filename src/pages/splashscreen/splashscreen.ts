de27d35134c8d6e63891a0b5a0a15295f34c36bd
Rasmus Nielsen <rasmus.andreas96@gmail.com>
2017-12-08 12:08:54 +0100

    fixes

--- src/pages/splashscreen/splashscreen.ts
+++ src/pages/splashscreen/splashscreen.ts
@@ -10,7 +10,6 @@ import { HomePage } from '../home/home';
 })
 export class SplashscreenPage {
 
-
   scannedShop: '';
 
   constructor(public navCtrl: NavController,
@@ -20,7 +19,7 @@ export class SplashscreenPage {
       this.nativeStorage.getItem('scannedShopone')
         .then(data => { if (data.scannedShop != '') {
           this.navCtrl.setRoot(MenuPage).then();
-        } else if (data.scannedShop = '') {
+        } else {
           this.navCtrl.setRoot(HomePage).then();
         }}, error => console.error(error));
 
