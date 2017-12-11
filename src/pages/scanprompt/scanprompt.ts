b7de42d4964aa054ef4a2ef679d9a98238a58a00
Rasmus Nielsen <rasmus.andreas96@gmail.com>
2017-12-11 09:42:03 +0100

    start working with data

--- src/pages/scanprompt/scanprompt.ts
+++ src/pages/scanprompt/scanprompt.ts
@@ -27,9 +27,9 @@ export class ScanpromptPage {
 
     this.nativeStorage.getItem('scannedResult')
       //.then(data => this.productData = data, error => console.error(error));
-      .then(data => productService.findProductData(data).subscribe(productInfo => {
+      .then(data => productService.findProductData(data)/*.subscribe(productInfo => {
         this.findProductData = productInfo;
-      }), error => console.error(error));
+      })*/, error => console.error(error));
 
     /*productService.findProductData().subscribe(productInfo => {
       this.findProductData = productInfo;
