15c633b6fccc8f293aa5c82c638fb25443f20ca2
Sebastian Buch <seba5513@easv365.dk>
2017-12-11 10:55:32 +0100

    never gonna give you up

--- src/providers/product-service/product-service.ts
+++ src/providers/product-service/product-service.ts
@@ -48,7 +48,6 @@ export class ProductServiceProvider {
     return Observable.create(observable => {
       this.getAllProducts().subscribe(allProducts => {
         let productFound = allProducts.find(p => p.barcodeID === products.barcodeID);
-          alert(productFound + productFound.productName + productFound.barcodeID);
           observable.next(productFound);
           observable.complete();
       })
