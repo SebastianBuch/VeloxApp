0d8963fe5b349807572d3e997d1bfcb725458bb2
Rasmus Nielsen <rasmus.andreas96@gmail.com>
2017-12-12 10:47:18 +0100

    for safety

--- src/pages/receipt/receipt.ts
+++ src/pages/receipt/receipt.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { NativeStorage } from '@ionic-native/native-storage';
 import { MenuPage } from '../menu/menu';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-receipt',
@@ -13,7 +14,8 @@ export class ReceiptPage {
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
-              private nativeStorage: NativeStorage) {
+              private nativeStorage: NativeStorage,
+              private storage: Storage) {
 
     this.nativeStorage.getItem('QRnativeData')
       .then(
