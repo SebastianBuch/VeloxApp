e9c9358f650e9f6f4625859c600e117f8e49d11c
Rasmus Nielsen <rasmus.andreas96@gmail.com>
2017-12-12 11:16:19 +0100

    for safety

--- src/providers/product-service/product-service.ts
+++ src/providers/product-service/product-service.ts
@@ -22,49 +22,7 @@ export class ProductServiceProvider {
     ];
   }
 
-  /*getAllProducts() :Observable<ProductData[]> {
-    return Observable.create(observable => {
-      this.storage.get('products').then(products => {
-        if (!products) {
-          this.products = [
-            {
-              barcodeID: '5701098025269',
-              productName: 'Brillerens'
-            },
-            {
-              barcodeID: '8719323928014',
-              productName: 'Sko'
-            }
-          ];
-          this.storage.set('products', products);
-        }
-        observable.next(products);
-        observable.complete()
-      });
-    });
-  }*/
-
   findProductData(): Observable<ProductData> {
-    /*return Observable.create(observable => {
-      this.getAllProducts().subscribe(allProducts => {
-        let productFound = allProducts.find(p => p.barcodeID === products.barcodeID);
-          observable.next(this.products.indexOf(productFound));
-          observable.complete();
-      })
-    })*/
-
-
-    /*return Observable.create(observable => {
-    let productFound = this.products.find(p => p.barcodeID === barcode);
-    if (productFound) {
-      observable.next(productFound);
-      observable.complete();
-    }*/
-    /*this.products.forEach((item, index) => {
-      if (item.barcodeID == barcode) {
-        return this.products[index];
-      }
-    });*/
     let productInfo = 0;
     return Observable.create(observable => {
       observable.next(this.products[productInfo]);
