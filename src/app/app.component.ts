dc55e8f90c5e3b80fbb562c8c949abea9c38d46c
Rasmus Nielsen <rasmus.andreas96@gmail.com>
2017-12-13 14:43:25 +0100

    I shall nou commit thou this shite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import {HomePage} from '../pages/home/home';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = SplashscreenPage;
+  rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
