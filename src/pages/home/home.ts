94376bb51ae3a3b784d2039fd29fa1cb1b8bb496
Rasmus Nielsen <rasmus.andreas96@gmail.com>
2017-12-13 21:21:24 +0100

    I shall nou commit thou this shite

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -29,10 +29,10 @@ export class HomePage {
               private statusService: StatusProvider) {
 
     this.nativeStorage.getItem('scannedShopone')
-      .then(data => { this.scannedShop = data }, /*error => alert(error)*/);
-    if (this.scannedShop != '') {
-      this.navCtrl.setRoot(MenuPage).then();
-    }
+      .then(data => { if (data.scannedShop != '') {
+        this.navCtrl.setRoot(MenuPage).then();
+    }}, /*error => alert(error)*/);
+
   }
 
   async scanBarcode() {
